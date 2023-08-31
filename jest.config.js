279c98bd445ecc832a1f88e3dcbff2924cea9bde
claralmdv <cchaouat@lesmaisonsduvoyage.com>
2023-08-31 11:08:15 +0200

    Jest set up & First exercice

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // ...
+    "transform": {
+      "^.+\\.jsx?$": "babel-jest",
+      "^.+\\.tsx?$": "ts-jest",
+      "^.+\\.js$": "babel-jest",
+      "^.+\\.mjs$": "babel-jest"
+    },
+    // ...
+  };
--- src/1_basics.test.js
+++ src/1_basics.test.js
@@ -4,16 +4,22 @@ import { multiply, isSmallerThan10 } from './1_basics';
 // https://www.youtube.com/watch?v=__QEPUdnJS0
 describe('1_Basics', () => {
   describe('multiply()', () => {
-    it('...', () => {
-      multiply(3, 5);
-      // your test here...
+    it('should return the product of two numbers', () => {
+      expect(multiply(3, 5)).toBe(15);
+      expect(multiply(0, 5)).toBe(0);
+      expect(multiply(-2, 4)).toBe(-8);
+     
     });
   });
 
   describe('isSmallerThan10()', () => {
-    it('Your tests here...', () => {
-      isSmallerThan10(5);
-      // your test here...
+    it('It should compare if the number is smaller than 10', () => {
+      expect(isSmallerThan10(5)).toBe('yes dah!');
+      expect(isSmallerThan10(10)).toBe('not really...');
+      expect(isSmallerThan10(11)).toBe('not really...');
+
     });
   });
 });
+
+
